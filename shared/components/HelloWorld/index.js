2b5b8a73432149ddb6835f344e81233e8d0fe504
Yansen Dou <sendy511@hotmail.com>
2017-06-13 13:48:52 +0800

    finish

---
+++ shared/components/HelloWorld/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './HelloWorld.css'
+
+function HelloWorld() {
+  return (
+    <div className={styles.background}>
+      hello, world!
+    </div>
+  )
+}
+
+export default HelloWorld
---
+++ src/helloWorld.spec.js
@@ -0,0 +1,20 @@
+import { arrowFunction } from './es6'
+import React from 'react'
+import HelloWorld from '@shared/components/HelloWorld/'
+import renderer from 'react-test-renderer';
+
+it('first test', () => {
+	expect(true, true)
+})
+
+it('second test with es6 syntax', () => {
+	expect('arrowFunction', arrowFunction())
+})
+
+it('third test with @shared alias', () => {
+  const component = renderer.create(
+    <HelloWorld />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+})
